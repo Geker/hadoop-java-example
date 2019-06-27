8c8359e05068693b7f4032a0cf2ca9bdeb472276
songqingqing <songqingqing@foxmail.com>
2019-06-27 16:26:40 +0800

    Add WordCount Example

---
+++ src/main/java/com/wordcount/WordCnt.java
@@ -0,0 +1,24 @@
+package com.wordcount;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCnt extends Reducer<LongWritable, Text, Text, IntWritable> {
+
+	protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+		int sum = 0;
+		Iterator<IntWritable> valuesIt = values.iterator();
+
+		while (valuesIt.hasNext()) {
+			sum = sum + valuesIt.next().get();
+		}
+
+		context.write(key, new IntWritable(sum));
+	}
+}
