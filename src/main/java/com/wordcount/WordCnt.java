58d216a78d28ce1c53bb0d87713a20fecb7be440
songqingqing <songqingqing@foxmail.com>
2019-06-27 16:47:34 +0800

    update reducer

--- src/main/java/com/wordcount/WordCnt.java
+++ src/main/java/com/wordcount/WordCnt.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCnt extends Reducer<LongWritable, Text, Text, IntWritable> {
+public class WordCnt extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-	protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+	protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
 
 		int sum = 0;
 		Iterator<IntWritable> valuesIt = values.iterator();
